6fbe353e26d3476ef4f3f5dcc48b6392bbdfd6d6
Shlomi Assaf <shlomiasaf@gmail.com>
2015-11-27 13:37:04 +0200

    post install of examples scripts.

---
+++ npm_postinstall.js
@@ -0,0 +1,24 @@
+require('shelljs/global');
+var fs = require('fs'),
+    path = require('path');
+
+function getDirectories(srcpath) {
+    return fs.readdirSync(srcpath).filter(function(file) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
+}
+
+var examplesPath = path.join(__dirname, 'examples');
+var examples = getDirectories(examplesPath);
+
+examples.forEach(function(p) {
+    try {
+        fs.accessSync(path.join(examplesPath, p, 'package.json') , fs.F_OK)
+        cd(path.join(examplesPath, p));
+        echo('Installing NPM packages for example "' + p + '"...');
+        exec('npm install');
+    }
+    catch(e) {}
+});
+
+cd(__dirname);
