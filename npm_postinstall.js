dc49ddedf578d768eea7fc79178d083b66af4a98
Shlomi Assaf <shlomiasaf@gmail.com>
2015-11-28 22:03:01 +0200

    fix post install bug

--- npm_postinstall.js
+++ npm_postinstall.js
@@ -3,13 +3,20 @@ var fs = require('fs'),
     path = require('path');
 
 function getDirectories(srcpath) {
-    return fs.readdirSync(srcpath).filter(function(file) {
-        return fs.statSync(path.join(srcpath, file)).isDirectory();
-    });
+    try {
+        return fs.readdirSync(srcpath).filter(function(file) {
+            return fs.statSync(path.join(srcpath, file)).isDirectory();
+        });
+    }
+    catch(ex) {
+        return null;
+    }
 }
 
 var examplesPath = path.join(__dirname, 'examples');
 var examples = getDirectories(examplesPath);
+
+if (!examples) return;
 
 examples.forEach(function(p) {
     try {
