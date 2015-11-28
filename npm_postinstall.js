695c2f5b6f6b016a6eede768eb3f4400f0755057
Shlomi Assaf <shlomiasaf@gmail.com>
2015-11-28 22:09:43 +0200

    fix post install bug

--- npm_postinstall.js
+++ npm_postinstall.js
@@ -1,31 +1,31 @@
-require('shelljs/global');
-var fs = require('fs'),
-    path = require('path');
+try {
+    require('shelljs/global');
+    var fs = require('fs'),
+        path = require('path');
 
-function getDirectories(srcpath) {
-    try {
+    function getDirectories(srcpath) {
         return fs.readdirSync(srcpath).filter(function(file) {
             return fs.statSync(path.join(srcpath, file)).isDirectory();
         });
     }
-    catch(ex) {
-        return null;
-    }
+
+    var examplesPath = path.join(__dirname, 'examples');
+    var examples = getDirectories(examplesPath);
+
+    if (!examples) return;
+
+    examples.forEach(function(p) {
+        try {
+            fs.accessSync(path.join(examplesPath, p, 'package.json') , fs.F_OK)
+            cd(path.join(examplesPath, p));
+            echo('Installing NPM packages for example "' + p + '"...');
+            exec('npm install');
+        }
+        catch(e) {}
+    });
+
+    cd(__dirname);
 }
+catch (ex) {
 
-var examplesPath = path.join(__dirname, 'examples');
-var examples = getDirectories(examplesPath);
-
-if (!examples) return;
-
-examples.forEach(function(p) {
-    try {
-        fs.accessSync(path.join(examplesPath, p, 'package.json') , fs.F_OK)
-        cd(path.join(examplesPath, p));
-        echo('Installing NPM packages for example "' + p + '"...');
-        exec('npm install');
-    }
-    catch(e) {}
-});
-
-cd(__dirname);
+}
